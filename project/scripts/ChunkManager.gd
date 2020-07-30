a89e31cc9e13441d05d46624b773d0e31c85025c
mdrgon1 <mdrgon1@umbc.edu>
2020-07-29 22:59:34 -0400

    created basic chunk management
    chunk manager holds on to an array of generated chunks (in terms of terrain for now, maybe generating other stuff later), and loaded chunks (added to the tree). generates chunks and loads them, not in any useful way right now

--- project/scripts/ChunkManager.gd
+++ project/scripts/ChunkManager.gd
@@ -1,3 +1,32 @@
 extends Node
 
+var generated_chunks : Array
+var chunks : Array
 
+func _ready():
+	
+	generated_chunks.resize(16)
+
+func _process(delta):
+	
+	# generate chunks, don't add them to tree just yet
+	for i in range(generated_chunks.size()):
+		var chunk : Chunk = generated_chunks[i]
+		if(chunk == null): # this is just to limit chunk generation to once a frame, TODO replace this with actual multithreading or something
+			chunk = Chunk.new()
+			chunk.heightmap = (load("res://heightmap.png"))
+			chunk.height = 5
+			chunk.translation.x = i * 15
+			chunk._generate_mesh()
+			
+			generated_chunks[i] = chunk
+			add_chunk(i)
+			
+			break
+	
+
+# add a generated chunk to the tree
+func add_chunk(chunk_idx : int):
+	
+	add_child(generated_chunks[chunk_idx])
+	chunks.push_back(generated_chunks[chunk_idx])
