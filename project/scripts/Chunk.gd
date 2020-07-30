d66e4a0336b9f976931502f44c4dffd8adb6c152
mdrgon1 <mdrgon1@umbc.edu>
2020-07-29 22:41:16 -0400

    moved chunk destructor into _notification()

--- project/scripts/Chunk.gd
+++ project/scripts/Chunk.gd
@@ -7,11 +7,12 @@ var terrain_gen = preload("res://bin/terrainGen.gdns").new()
 export var heightmap : Texture
 export var height : float
 
-func _ready():
+func _generate_mesh():
 	terrain_gen.generate_mesh_from_heightmap(heightmap.get_data(), height)
 	mesh.set_mesh(terrain_gen.get_mesh())
 	
 	add_child(mesh)
 
-func _exit_tree():
-	mesh.free()
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		mesh.free()
