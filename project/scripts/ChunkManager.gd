f745c770f93a8ee3eb3bbee50100e194d382c1ec
mdrgon1 <mdrgon1@umbc.edu>
2020-07-31 23:24:25 -0400

    moved chunk generation into generate_chunk()

--- project/scripts/ChunkManager.gd
+++ project/scripts/ChunkManager.gd
@@ -13,18 +13,25 @@ func _process(delta):
 	
 	# generate chunks, don't add them to tree just yet 
 	if(generation_queue.size() != 0):# this is just to limit chunk generation to once a frame, TODO replace this with actual multithreading or something
-		var chunk = Chunk.new()
+		
 		var coords = generation_queue.pop_front()
+		generate_chunk(coords)
 		
-		chunk.heightmap = preload("res://icon.png").get_data().get_rect(Rect2(coords * 15, Vector2(16, 16)))
-		chunk.height = 5
-		chunk.translation.x = coords.x * 15
-		chunk.translation.y = -10
-		chunk.translation.z = coords.y * 15
-		chunk._generate_mesh()
-		
-		generated_chunks[coords] = chunk
 		add_chunk(coords)
+
+# generate terrain geometry
+# does not add the chunk to the scenetree, just creates all the data
+func generate_chunk(coords : Vector2):
+	var chunk = Chunk.new()
+	
+	chunk.heightmap = preload("res://heightmap.png").get_data().get_rect(Rect2(coords * 15, Vector2(16, 16)))
+	chunk.height = 5
+	chunk.translation.x = coords.x * 15
+	chunk.translation.y = -10
+	chunk.translation.z = coords.y * 15
+	chunk._generate_mesh()
+	
+	generated_chunks[coords] = chunk
 
 # add a generated chunk to the tree
 func add_chunk(coords : Vector2):
