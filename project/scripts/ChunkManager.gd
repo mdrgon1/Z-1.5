b9c0111d59b9c3273f34835a173a089edcd191c9
mdrgon1 <mdrgon1@umbc.edu>
2020-08-07 22:15:14 -0400

    added chunk pooling

--- project/scripts/ChunkManager.gd
+++ project/scripts/ChunkManager.gd
@@ -1,11 +1,17 @@
 extends Node
 
+const INITIAL_NUM_CHUNKS_POOLED := 8
+
+var chunk_pool : Array
 var loading_queue : Array
 var loaded_chunks : Dictionary
 var chunks : Dictionary
 var heightmap : Image = preload("res://heightmap.png").get_data()
 
 func _ready():
+	for i in INITIAL_NUM_CHUNKS_POOLED:
+		chunk_pool.push_back(Chunk.new())
+	
 	for i in 16:
 		var coordinates := Vector2(i % 4, int(i / 4))
 		loading_queue.push_back(coordinates)
@@ -23,7 +29,12 @@ func _process(delta):
 # generate terrain geometry
 # does not add the chunk to the scenetree, just creates all the data
 func load_chunk(coords : Vector2) -> void:
-	var chunk : Chunk = Chunk.new()
+	# pull chunk from pool if possible
+	var chunk : Chunk
+	if(chunk_pool.size() == 0):
+		chunk = Chunk.new()
+	else:
+		chunk = chunk_pool.pop_back()
 	
 	chunk.heightmap = heightmap.get_rect(Rect2(coords * 15, Vector2(16, 16)))
 	chunk.height = 5
@@ -46,6 +57,10 @@ func add_chunk(coords : Vector2) -> void:
 # remove a chunk from the tree
 func remove_chunk(coords : Vector2) -> void:
 	remove_child(chunks[coords])
+	
+	# chunks are returned to the pool after deletion, even if they didn't come from there
+	chunk_pool.push_back(chunks[coords])
+	
 	chunks.erase(coords)
 
 # delete the chunk from memory
